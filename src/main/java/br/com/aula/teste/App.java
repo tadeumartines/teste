cbfcddb85d87d2cbe66e966fc619286c86b66e1a
Rodrigo Bonadia <rodrigo.bonadia@hotmail.com>
2019-05-22 21:48:27 -0300

    commit teste2

--- src/main/java/br/com/aula/teste/App.java
+++ src/main/java/br/com/aula/teste/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "Melhor aula de quarta-feira a noite" );
         System.out.println( "Melhor aula" );
         System.out.println( "O professor Plinio é o melhor" );
     }
